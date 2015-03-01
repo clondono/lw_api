5f17cb561e26910f646a10af2c4c5fcaafedb49c
clondono <christianlondono91@gmail.com>
2015-03-01 05:43:08 -0600

    created base api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LwApi::Application.config.secret_key_base = '5f5cb5e60e9f8b51823fc478a6980147fccf66b33c080ff1794d716781f88d792479c88d97a8b69a60e78b3938d4eb67f6d11da65d509bea692e1086cc024ca2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LwApi::Application.config.session_store :cookie_store, key: '_lw_api_session'
