9c8876bcb056bdf287b51cdee07a086bf9b391b4
clondono <christianlondono91@gmail.com>
2015-03-03 16:13:09 -0600

    trying to set up api for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 LwApi::Application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }  do
   	post '/', to: 'api_endpoint#create'
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
